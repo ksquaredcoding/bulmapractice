65659fbbb8fe343a9cd74435807e9e4296d1f7f5
ksquaredcoding <ksquaredcoding@gmail.com>
2023-01-10 15:58:26 -0700

    removed template items

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,11 @@
 <script setup>
 import BulmaTest from "../components/BulmaTest.vue";
 import ModalButton from "../components/ModalButton.vue";
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
     <BulmaTest />
-    <TheWelcome />
     <ModalButton />
   </main>
 </template>
